4b109c936a6ffba39b4be0aec7bebc74903972af
Tim <timson995@gmail.com>
2021-05-24 15:08:45 +0600

    Добавьте файлы проекта.

---
+++ 24_05_EFSOL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24_05_EFSOL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
