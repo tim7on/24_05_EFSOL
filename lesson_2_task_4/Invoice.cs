b9029719a32cc37e19278ccf4861d63bbed6cda5
Bakai <thedesport20@gmail.com>
2021-05-24 16:32:31 +0600

    12

---
+++ lesson_2_task_4/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2_task_4
+{
+    class Invoice
+    {
+        public readonly int account;
+        private readonly string customer;
+        private readonly string provider;
+
+    }
+}
---
+++ lesson_2_task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_2_task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
