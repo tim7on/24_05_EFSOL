92a57a16493aa8ef1772ee62d54175041ef47379
Bakai <thedesport20@gmail.com>
2021-05-24 18:19:14 +0600

    dfg

--- lesson_2_task_3/Employee_1.cs
+++ lesson_2_task_3/Employee_1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace lesson_2_task_3
 {
@@ -9,11 +10,62 @@ namespace lesson_2_task_3
             Console.WriteLine();
         }
 
-        private int whatPosition(string position)
+        private int WhatPosition(string position)
         {
-            int temp;
+            int temp = 0;
+            switch (position)
+            {
+                case ("финансовый директор"): temp = 1;
+                    break;
+                case ("технический директор"): temp = 2;
+                    break;                
+                case ("директор по производству и выпуску продукции"): temp = 3;
+                    break;                
+                case ("главный инженер"): temp = 4;
+                    break;                
+                case ("руководитель кадрового отдела"): temp = 5;
+                    break;                
+                case ("главный бухгалтер"): temp = 6;
+                    break;                
+                case ("руководитель отдела торговли"): temp = 7;
+                    break;                
+                case ("руководитель отдела закупок"): temp = 8;
+                    break;                
+                case ("руководитель отдела по связям с общественностью"): temp = 9;
+                    break;
+            }
 
-            return 1;
+            return temp;
+        }
+
+        private double WhatExperience(int exp)
+        {
+            double 
+
+            if (exp < 5 && exp >= 1)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.10}");
+            }
+            else if (exp >= 5 && exp < 10)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.15}");
+            }
+            else if (exp >= 10 && exp < 15)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.25}");
+            }
+            else if (exp >= 15 && exp < 20)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.35}");
+            }
+            else if (exp >= 20 && exp < 25)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.45}");
+            }
+            else if (exp >= 25)
+            {
+                Console.WriteLine($"Ваша з/п с учетом премий: {salary * 1.50}");
+            }
         }
     }
 }
