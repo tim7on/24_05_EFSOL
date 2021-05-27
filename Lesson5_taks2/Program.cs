9b0a442e378cbc5af82116479e06fce9b6a91389
Tim <timson995@gmail.com>
2021-05-27 18:43:08 +0600

    finished5_2

---
+++ Lesson5_taks2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+/*Задание 2 
+Используя Visual Studio, создайте проект по шаблону Console Application. 
+Требуется: 
+Создать массив размерностью N элементов, заполнить его произвольными целыми значениями. 
+Вывести наибольшее значение массива, наименьшее значение массива, общую сумму элементов, 
+среднее арифметическое всех элементов, вывести все нечетные значения. */
+namespace Lesson5_taks2
+{
+    class ArrayActions
+    {
+        public static int ArrayMin(Array x)
+        {
+            int min = 99999;
+            foreach (int number in x)
+            {
+                min = min < number ? min : number;
+            }
+            return min;
+        }
+        public static int ArrayMax(Array x)
+        {
+            int max = 0;
+            foreach (int number in x)
+            {
+                max = max > number ? max : number;
+            }
+            return max;
+        }
+        public static int ArraySum(Array x)
+        {
+            int sum = 0;
+            foreach (int number in x)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+        public static int ArrayAverage(Array x)
+        {
+            int result = ArraySum(x) / x.Length;
+            return result;
+        }
+         public static void ArrayEven(Array x)
+        {
+            Console.Write("Even:\t");
+            foreach (int number in x)
+            {
+                string i = number % 2 == 0 ?  $"{number}," : "";
+                Console.Write("{0}", i);
+            }
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input Lenght of Array");
+                bool inti = int.TryParse(Console.ReadLine(), out int n);
+                if (inti)
+                {
+                    int [] array = new int[n];
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Random rnd = new Random();
+                        array[i] = rnd.Next(0,999);
+                        Console.WriteLine(array[i]);
+                    }
+                    Console.WriteLine("Minimum: {0}",ArrayActions.ArrayMin(array));
+                    Console.WriteLine("Maximum: {0}", ArrayActions.ArrayMax(array));
+                    Console.WriteLine("Sum: {0}", ArrayActions.ArraySum(array));
+                    Console.WriteLine("Average: {0}", ArrayActions.ArrayAverage(array));
+                    ArrayActions.ArrayEven(array);
+                    Console.WriteLine(new string('-', 25));
+                }
+            }
+
+        }
+    }
+}
